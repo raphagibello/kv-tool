efbf38277cc3dc4a7c5326bbd83ba9e9ed784cfc
JoaoVitorMachadoAndradeSousa <outlast4321@gmail.com>
2021-06-21 21:40:58 -0300

    encode and decode lines
    creating encoding and decoding methods

--- src/commands/base64_handler.py
+++ src/commands/base64_handler.py
@@ -3,14 +3,24 @@ from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential,AzureCliCredential
 import click
 
-def encode_line():
-    return 0
+def encode_line(message):
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode((message_bytes))
+    base64_message = base64_bytes.decode('ascii')
+
+    return base64_message  # returns the encoded String
 
 def encode_file():
     return 0
 
-def decode_line():
-    return 0
+def decode_line(message):
+    encoded = encode_line(message)  #encoded String
+    base64_message = encoded
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+
+    return message  #returns the decoded String
 
 def decode_file():
     return 0
