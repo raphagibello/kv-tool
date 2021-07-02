28659b354a2a5497fe8852122c19a4f357467e57
JoaoVitorMachadoAndradeSousa <outlast4321@gmail.com>
2021-07-02 11:46:21 -0300

    adding files encoding and decoding

--- src/commands/base64_handler.py
+++ src/commands/base64_handler.py
@@ -11,7 +11,11 @@ def encode_line(message):
     return base64_message  # returns the encoded String
 
 def encode_file():
-    return 0
+    with open('random.txt', 'rb') as binary_file:
+        binary_file_data = binary_file.read()
+        base64_encoded_data = base64.b64encode(binary_file_data)
+        base64_message = base64_encoded_data.decode('utf-8')
+        print(base64_message)
 
 def decode_line(message):
     encoded = encode_line(message)  #encoded String
@@ -22,8 +26,11 @@ def decode_line(message):
 
     return message  #returns the decoded String
 
-def decode_file():
-    return 0
+def decode_file(key):
+    base64_img_bytes = key.encode('utf-8')
+    with open('random.txt', 'wb') as file_to_save:
+        decoded_image_data = base64.decodebytes(base64_img_bytes)
+        file_to_save.write(decoded_image_data)
 
 # with open("secrets_k8s_decoded.txt","r") as decoded_file:
 #     for line in decoded_file:
